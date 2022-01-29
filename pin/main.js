af829f7cd013e74ae2f9de2ed92a6c2aa1d6e02a
DICTATOR-DEV <98098372+DICTATOR-DEV@users.noreply.github.com>
2022-01-29 19:57:50 +0530

    hi
    hi

--- pin/main.js
+++ pin/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
-    //change selectboxes to selectize mode to be searchable
-       $("select").select2();
-    });
+   //change selectboxes to selectize mode to be searchable
+      $("select").select2();
+   });
+   
